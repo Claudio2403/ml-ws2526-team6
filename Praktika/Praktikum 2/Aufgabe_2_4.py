85aaac1f343424f05590558d1e9a6e53961f9f70
lucamalcomartens <martens.lucamalcogmx.de>
2025-12-06 15:21:52 +0100

    Achsenabschnittsvergleich ergänzt

--- Praktika/Praktikum 2/Aufgabe_2_4.py
+++ Praktika/Praktikum 2/Aufgabe_2_4.py
@@ -7,5 +7,8 @@ X_sklearn = aufgabe2_2.x.reshape(-1, 1)
 reg = LinearRegression()
 reg.fit(X_sklearn, aufgabe2_2.y)
 
+print(f"Unser Achsenabschnitt (Beta_0):   {aufgabe2_2.beta[0]}")
+print(f"Scikit-Learn Intercept:          {reg.intercept_}")
+
 print(f"Unsere Implementierung (Beta_1): {aufgabe2_2.beta[1]}")
 print(f"Scikit-Learn (Coef):             {reg.coef_[0]}")
