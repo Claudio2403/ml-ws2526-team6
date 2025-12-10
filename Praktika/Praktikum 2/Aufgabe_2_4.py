dab20e2a8d38b1bccf97a1e224fb3bb978b74045
lucamalcomartens <martens.lucamalcogmx.de>
2025-12-10 12:17:59 +0100

    import gekillt

--- Praktika/Praktikum 2/Aufgabe_2_4.py
+++ Praktika/Praktikum 2/Aufgabe_2_4.py
@@ -1,8 +1,7 @@
-import numpy as np
 from sklearn.linear_model import LinearRegression
 import Aufgabe_2_2 as aufgabe2_2
 
-X_sklearn = aufgabe2_2.x.reshape(-1, 1)
+X_sklearn = aufgabe2_2.x.reshape(-1, 1) 
 
 reg = LinearRegression()
 reg.fit(X_sklearn, aufgabe2_2.y)
